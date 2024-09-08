78767917ddd0d14265e471c929e98e06d657c05b
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-09-08 12:00:59 +0300

    frist

---
+++ main.js
@@ -0,0 +1,59 @@
+
+// Q1
+let length =5;
+let width =3
+
+let area = length * width 
+console.log(area);
+
+//Q2
+
+let cel = 20; 
+let fahr = (cel * 9/5) + 32;
+console.log(`${cel}°C ${fahr}°F`);
+fahr = 50; 
+cel = (fahr - 32) * 5/9;
+console.log(`${fahr}°F  ${cel}°C`);
+
+
+
+// Q3
+
+let hour = 1; 
+let second = hour * 600; 
+console.log(`${hour} hour = ${second} second`);
+
+//Q4
+
+let thingOne = "أكاديمية"; 
+let thingTwo = "طويق"; 
+console.log(thingOne + " " + thingTwo);
+
+
+
+//Q5
+let bill = 10.25 + 3.99 + 7.15;
+let tip = bill * 0.15; 
+let total = bill + tip; 
+console.log(total); 
+
+
+
+
+//Q6
+
+
+let adjective1 = "amazing";
+let adjective2 = "fun";
+let adjective3 = "entertaining";
+let madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+console.log(madLib); 
+
+
+
+//Q7
+let firstName = "Arwa"; 
+let interest = "JavaScript";
+let hobby = "cook sweet"; 
+let awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + ".";
+console.log(awesomeMessage); 
